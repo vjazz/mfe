7ad1ed98d7c9127c5aaeb1c263ee429da99b6a6c
Vinayak Jaiswal <vinayak@Mac.lan>
2025-12-01 20:53:52 +0530

    add some code to check git workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello Vinayak J</h1>
+      <h1>Hello Vinayak</h1>
       <hr />
       <MarketingApp />
     </div>
